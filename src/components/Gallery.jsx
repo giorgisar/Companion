dcc19831a4f1d919faaf8b860fb56c3799780a79
giorgisar <cocanidzegiorgi66@gmail.com>
2023-07-11 21:22:10 +0400

    message

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div id="gallery" className="max-w-[1140px] m-auto w-full p-4 py-16">
+      <h2 className="text-center text-gray-700 p-4">Gallery</h2>
+      <div className="grid sm:grid-cols-5 gap-4">
+        <div className="sm:col-span-3 col-span-2 row-span-2">
+          <img
+            className="w-full h-full object-cover "
+            src="https://images.unsplash.com/photo-1535262412227-85541e910204?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover "
+            src="https://images.unsplash.com/photo-1540541338287-41700207dee6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover "
+            src="https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1349&q=80"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover "
+            src="https://images.unsplash.com/photo-1596178065887-1198b6148b2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-full object-cover "
+            src="https://images.unsplash.com/photo-1596436877314-0091e94a7ce7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
